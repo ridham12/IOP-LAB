9e0b4a3cd3ceae156102f25dacdb03b174430184
ridham12 <84904732+ridham12@users.noreply.github.com>
2026-09-19 13:50:05 +0530

    Delete LAB 1 directory

--- LAB 1/assn4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    float farh, cel;
-    printf("Enter temperature in Fahrenheit: ");
-    scanf("%f", &farh);
-    cel = (farh - 32) * 5 / 9;
-    printf("Temperature in Celsius: %.2f", cel);
-    return 0;
-}
